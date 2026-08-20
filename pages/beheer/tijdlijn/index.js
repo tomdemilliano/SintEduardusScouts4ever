815c096fc481168f7925cd8d27ad01b556935150
tomdemilliano <tomdemilliano@gmail.com>
2026-08-20 11:07:42 +0200

    Add new tabs for Takken and Leidingsploegen

--- pages/beheer/tijdlijn/index.js
+++ pages/beheer/tijdlijn/index.js
@@ -8,6 +8,8 @@ import AdminSubNav from '../../../components/AdminSubNav';
 const TABS = [
   { href: '/beheer/tijdlijn', label: '🚩 Mijlpalen', exact: true },
   { href: '/beheer/tijdlijn/kentekens', label: '🧭 Kentekens' },
+  { href: '/beheer/tijdlijn/takken', label: '👥 Takken' },
+  { href: '/beheer/tijdlijn/leiding', label: 'Leidingsploegen' },
 ];
 
 export default function MijlpalenPage() {
