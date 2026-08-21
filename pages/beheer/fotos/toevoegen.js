e2d952985e1b49bf021468475a026be22c90ab35
tomdemilliano <tomdemilliano@gmail.com>
2026-08-21 15:04:42 +0200

    Add 'Dubbels' tab to the photo management page

--- pages/beheer/fotos/toevoegen.js
+++ pages/beheer/fotos/toevoegen.js
@@ -10,6 +10,7 @@ const TABS = [
   { href: '/beheer/fotos', label: 'Overzicht', exact: true },
   { href: '/beheer/fotos/toevoegen', label: "+ Foto's toevoegen" },
   { href: '/beheer/fotos/tags', label: 'Tags' },
+  { href: '/beheer/fotos/dubbels', label: '🔍 Dubbels' },
 ];
 
 const STATUS_LABEL = {
