0442b6964a5d2c52484f959413c46d047db6563e
tomdemilliano <tomdemilliano@gmail.com>
2026-08-26 13:49:28 +0200

    Add 'Wijzigingsvoorstellen' tab to upload page

--- pages/beheer/vriendenboek/upload.js
+++ pages/beheer/vriendenboek/upload.js
@@ -10,6 +10,7 @@ const TABS = [
   { href: '/beheer/vriendenboek', label: 'Overzicht', exact: true },
   { href: '/beheer/vriendenboek/upload', label: '+ Eén scan' },
   { href: '/beheer/vriendenboek/bulk-upload', label: '+ Meerdere scans' },
+  { href: '/beheer/vriendenboek/wijzigingen', label: '✏️ Wijzigingsvoorstellen' },
 ];
 
 export default function UploadPage() {
